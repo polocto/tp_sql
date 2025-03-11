8e8411d52e9a6e3576bc3111c632a2975f89c86b
Paul Sade <paul.sade@live.fr>
2025-03-11 17:36:56 +0100

    Database modeling updated

--- school.sql
+++ school.sql
@@ -3,23 +3,22 @@ CREATE DATABASE school;
 USE school;
 
 CREATE TABLE students (
+    id VARCHAR(50) PRIMARY KEY,
     last_name VARCHAR(50),
     first_name VARCHAR(20),
-    school_year INTEGER NOT NULL,
-    CONSTRAINT student PRIMARY KEY (last_name, first_name)
+    school_year INTEGER NOT NULL
 );
 
 CREATE TABLE courses (
-    title VARCHAR(50) PRIMARY KEY,
+    id VARCHAR(50) PRIMARY KEY,
+    title VARCHAR(50),
     school_year TINYINT NOT NULL,
-    duration INTEGER NOT NULL
+    duration INTEGER NOT NULL,
+    CONSTRAINT course UNIQUE (title, school_year)
 );
 
 CREATE TABLE enrollements (
-    student_last_name VARCHAR(50),
-    student_first_name VARCHAR(20),
-    course_title VARCHAR(50) REFERENCES courses(title),
-    final_grade REAL DEFAULT NULL,
-
-    FOREIGN KEY (student_last_name, student_first_name) REFERENCES students(last_name, first_name)
+    student_id VARCHAR(50) REFERENCES students(id)
+    course_id VARCHAR(50) REFERENCES courses(id),
+    final_grade REAL DEFAULT NULL
 );
