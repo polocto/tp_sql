fe4266a22cb1546f913baf242131fcc7d3f34c21
Paul Sade <paul.sade@live.fr>
2025-03-11 15:35:15 +0100

    Database modeling

---
+++ school-populate.sql
@@ -0,0 +1,13 @@
+USE school;
+
+INSERT INTO students(`last_name`,`first_name`,`school_year`)
+VALUES (
+'SADE', 'PAUL',6
+);
+
+
+INSERT INTO courses
+VALUES ('Database',6,27);
+
+INSERT INTO enrollements
+VALUES ('SADE', 'PAUL', 'Database', 12.5)
--- school.sql
+++ school.sql
@@ -5,21 +5,21 @@ USE school;
 CREATE TABLE students (
     last_name VARCHAR(50),
     first_name VARCHAR(20),
-    school_year INTEGER,
+    school_year INTEGER NOT NULL,
     CONSTRAINT student PRIMARY KEY (last_name, first_name)
 );
 
 CREATE TABLE courses (
     title VARCHAR(50) PRIMARY KEY,
-    school_year INTEGER,
-    duration DATETIME
+    school_year TINYINT NOT NULL,
+    duration INTEGER NOT NULL
 );
 
 CREATE TABLE enrollements (
     student_last_name VARCHAR(50),
     student_first_name VARCHAR(20),
     course_title VARCHAR(50) REFERENCES courses(title),
-    final_grade REAL
+    final_grade REAL DEFAULT NULL,
 
     FOREIGN KEY (student_last_name, student_first_name) REFERENCES students(last_name, first_name)
 );
